f754d0239c86e0721e6a14b9f4efee10e95789cf
DarwinJS <DarwinJS@sanoys.com>
2015-07-17 07:38:13 -0400

    fix for moved file source

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $Package = "wkhtmltopdf"
 $Version = "0.12.2"
 
 $installerType = 'EXE'
-$url = 'http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.2_msvc2013-win32.exe'
-$url64 = 'http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.2_msvc2013-win64.exe'
+$url = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.2.4/wkhtmltox-0.12.2.4_msvc2013-win32.exe'
+$url64 = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.2.4/wkhtmltox-0.12.2.4_msvc2013-win64.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
